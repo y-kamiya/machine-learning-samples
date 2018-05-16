2896e5bc324ecd4564bb6d2f42ece77d9013b42b
Yuji Kamiya <y.kamiya0@gmail.com>
2018-05-16 22:48:09 +0900

    cartpole dqn

---
+++ python3/enforcement/cartpole_dqn.py
@@ -0,0 +1,206 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import gym
+import numpy as np
+from collections import namedtuple 
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+ENV = 'CartPole-v0'
+NUM_DIGITIZE = 6
+GAMMA = 0.99
+ETA = 0.5
+MAX_STEPS = 200
+NUM_EPISODE = 1000
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.index = 0
+
+    def push(self, state, action, state_next, reward):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+
+        self.memory[self.index] = Transition(state, action, state_next, reward)
+        self.index = (self.index + 1) % self.capacity
+
+    def sample(self, size):
+        return random.sample(self.memory, size)
+
+    def __len__(self):
+        return len(self.memory)
+
+import random
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+BATCH_SIZE = 32
+CAPACITY = 10000
+
+class Net(nn.Module):
+    def __init__(self, num_states, num_actions):
+        super(Net, self).__init__()
+        self.num_states = num_states
+        self.num_actions = num_actions
+
+        self.fc1 = nn.Linear(self.num_states, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, self.num_actions)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+class Brain:
+    def __init__(self, num_states, num_actions):
+        self.num_states = num_states
+        self.num_actions = num_actions
+
+        self.memory = ReplayMemory(CAPACITY)
+
+        self.model = Net(num_states, num_actions)
+        print(self.model)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.0001)
+    
+    def reply(self):
+        if (len(self.memory) < BATCH_SIZE):
+            return
+
+        transitions = self.memory.sample(BATCH_SIZE)
+
+        batch = Transition(*zip(*transitions))
+
+        non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None, batch.next_state)))
+
+        state_batch = Variable(torch.cat(batch.state))
+        action_batch = Variable(torch.cat(batch.action))
+        reward_batch = Variable(torch.cat(batch.reward))
+        non_final_next_state = Variable(torch.cat([s for s in batch.next_state if s is not None]))
+        
+        self.model.eval()
+
+        state_action_values = torch.squeeze(self.model(state_batch).gather(1, action_batch))
+
+        next_state_values = Variable(torch.zeros(BATCH_SIZE).type(torch.FloatTensor))
+        next_state_values[non_final_mask] = self.model(non_final_next_state).data.max(1)[0]
+
+        expected_state_action_values = reward_batch + GAMMA * next_state_values
+        
+        self.model.train()
+
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def decide_action(self, state, episode):
+        epsilon = 0.5 / (1 / (episode + 1))
+
+        if epsilon < random.uniform(0, 1):
+            self.model.eval()
+            action = self.model(Variable(state)).data.max(1)[1].view(1, 1)
+        else:
+            action = torch.LongTensor([[random.randrange(self.num_actions)]])
+
+        return action
+
+class Agent:
+    def __init__(self, num_states, num_actions):
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self.brain = Brain(num_states, num_actions)
+
+    def update_q_function(self):
+        self.brain.reply()
+
+    def get_action(self, state, step):
+        return self.brain.decide_action(state, step)
+
+    def memory(self, state, action, state_next, reward):
+        return self.brain.memory.push(state, action, state_next, reward)
+        
+class Environment:
+    def __init__(self):
+        self.env = gym.make(ENV)
+        self.num_states = self.env.observation_space.shape[0]
+        self.num_actions = self.env.action_space.n
+        self.agent = Agent(self.num_states, self.num_actions)
+        self.total_step = np.zeros(10)
+
+    def run(self):
+        complete_episodes = 0
+        episode_final = False
+
+        for episode in range(NUM_EPISODE):
+            observation = self.env.reset()
+            state = torch.from_numpy(observation).type(torch.FloatTensor)
+            state = torch.unsqueeze(state, 0)
+
+            for step in range(MAX_STEPS):
+                if episode_final:
+                    frames.append(self.env.render(mode='rgb_array'))
+
+                action = self.agent.get_action(state, episode)
+
+                observation_next, _, done, _ = self.env.step(action.item())
+
+                if done:
+                    state_next = None
+                    self.total_step = np.hstack((self.total_step[1:], step + 1))
+                    if step < 195:
+                        reward = torch.FloatTensor([-1.0])
+                        complete_episodes = 0
+                    else:
+                        reward = torch.FloatTensor([1.0])
+                        complete_episodes = complete_episodes + 1
+
+                else:
+                    reward = torch.FloatTensor([0.0])
+                    state_next = torch.from_numpy(observation_next).type(torch.FloatTensor)
+                    state_next = torch.unsqueeze(state_next, 0)
+
+                self.agent.memory(state, action, state_next, reward)
+
+                self.agent.update_q_function()
+
+                state = state_next
+
+                if done:
+                    print('episode: {0}, step: {1}'.format(episode, self.total_step.mean()))
+                    break
+
+                if episode_final:
+                    # display_frames_as_gif(frames)
+                    break
+
+                if 10 <= complete_episodes:
+                    print('success 10 times in sequence')
+                    frames = []
+                    episode_final = True
+                    
+        self.env.close()
+        
+if __name__ == '__main__':
+    # frames = []
+    # env = gym.make(ENV)
+    # env.reset()
+    # for _ in range(0, 200):
+    #     frames.append(env.render(mode='rgb_array'))
+    #     action = np.random.choice(2)
+    #     observation, reward, done, info = env.step(action)
+    #     if done:
+    #         break
+    #
+    # env.close()
+    # display_frames_as_gif(frames)
+    env = Environment()
+    env.run()
+
