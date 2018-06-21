3577068ac7fce18a6bc5e8061dd7302d92798089
Yuji Kamiya <y.kamiya0@gmail.com>
2018-06-21 21:25:03 +0900

    policy network for maze (debug)

---
+++ python3/enforcement/maze_policy_nn.py
@@ -0,0 +1,163 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+NUM_STATE = 8
+NUM_ACTION = 4
+NUM_STEPS = 100
+NUM_EPISODE = 2
+GAMMA = 0.99
+GOAL = 8
+
+class Net(nn.Module):
+    def __init__(self, num_states, num_actions):
+        super(Net, self).__init__()
+        self.num_states = num_states
+        self.num_actions = num_actions
+
+        self.fc1 = nn.Linear(self.num_states, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, self.num_actions)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return F.softmax(self.fc3(x))
+
+class Environment:
+    def __init__(self):
+        self.model = Net(NUM_STATE, NUM_ACTION)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+
+    def create_input(self, state):
+        e = torch.zeros([NUM_STATE], requires_grad=True)
+        e[state] = 1
+        return e
+
+    def get_next_state(self, state, action):
+        if action == 0:
+           s_next = state - 3
+        elif action == 1:
+           s_next = state + 1
+        elif action == 2:
+           s_next = state + 3
+        elif action == 3:
+           s_next = state - 1
+
+        if s_next < 0 or NUM_STATE < s_next: 
+            return state
+
+        return s_next
+
+    def get_next_action(self, state):
+        self.model.eval()
+        return self.model(self.create_input(state)).argmax().item()
+
+    def discount_reward(self, rewards):
+        discounted_rewards = np.zeros((rewards.size, NUM_ACTION))
+        running_add = 0
+        for i in range(rewards.size)[::-1]:
+            running_add = running_add * GAMMA + rewards[i]
+            for j in range(0, NUM_ACTION):
+                discounted_rewards[i][j] = running_add
+
+        return discounted_rewards
+
+    def run_to_goal(self):
+        state = 0
+        history = []
+
+        self.model.eval()
+
+        for step in range(0, NUM_STEPS):
+           props = self.model(self.create_input(state))
+           action = np.random.choice(range(0, NUM_ACTION), p=props.data.numpy())
+
+           next_state = self.get_next_state(state, action)
+           # print("state: {0}, action: {1}, next:{2}".format(state, action, next_state))
+
+           ys = np.zeros(NUM_ACTION)
+           ys[action] = 1
+
+           history.append([state, action, 0.0, props.data.numpy(), ys])
+           if step == NUM_STEPS:
+               history[-1][2] = -1.0
+               break
+
+           if next_state == GOAL:
+               history[-1][2] = 1.0
+               break
+
+           state = next_state
+
+        return history
+
+    def update_policy(self, history, episode):
+        self.model.eval()
+        aaa = self.model(self.create_input(1))
+
+        self.model.train()
+
+        rewards = np.zeros((len(history)))
+        props = np.zeros((len(history), NUM_ACTION))
+        targets = np.zeros((len(history), NUM_ACTION))
+        for i, entry in enumerate(history):
+            rewards[i] = entry[2]
+            props[i] = entry[3]
+            targets[i] = entry[4]
+            
+        discounted_rewards = self.discount_reward(rewards)
+        targets = targets * discounted_rewards
+
+        props.reshape(-1, NUM_ACTION)
+        targets.reshape(-1, NUM_ACTION)
+        # props = Variable(torch.tensor(props), requires_grad=True)
+        # targets = Variable(torch.tensor(targets), requires_grad=True)
+        props = Variable(torch.tensor(aaa), requires_grad=True)
+        targets = Variable(torch.tensor(aaa), requires_grad=True)
+        loss = F.l1_loss(props, targets)
+        self.optimizer.zero_grad()
+        loss.backward()
+
+        print("aaaaaaaa")
+        print(props)
+        print("bbbbbbbbbb")
+        print(targets)
+        print("ccccccccccc")
+        print(loss)
+        for param in self.model.parameters():
+            print(param.grad)
+
+        import pdb; pdb.set_trace()
+
+        self.optimizer.step()
+        
+
+    def display_model(self, episode):
+        print("episode: {0}".format(episode))
+        for i in range(0, NUM_STATE):
+            print(self.model(self.create_input(i)))
+
+    def run(self):
+        for episode in range(NUM_EPISODE):
+            state = 0
+
+            history = self.run_to_goal()
+            self.update_policy(history, episode)
+
+            self.model.eval()
+            # if episode % 10 == 0:
+            self.display_model(episode)
+
+
+if __name__ == '__main__':
+    env = Environment()
+    env.run()
+
