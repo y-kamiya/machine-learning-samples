13478d775902dc6e9d243700411b1aee100cf7e4
Yuji Kamiya <y.kamiya0@gmail.com>
2018-08-18 11:49:14 +0900

    model for dueling network

---
+++ python3/reinforcement/model.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from torch import nn
+import torch.nn.functional as F
+
+class NetFC(nn.Module):
+    def __init__(self, num_states, num_actions):
+        super(NetFC, self).__init__()
+        self.num_states = num_states
+        self.num_actions = num_actions
+
+        self.fc1 = nn.Linear(self.num_states, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, self.num_actions)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+class DuelingNetFC(nn.Module):
+    def __init__(self, num_states, num_actions):
+        super(DuelingNetFC, self).__init__()
+        self.num_states = num_states
+        self.num_actions = num_actions
+
+        self.fc1 = nn.Linear(self.num_states, 32)
+        self.fc2 = nn.Linear(32, 32)
+
+        self.fcV = nn.Linear(32, 1)
+        self.fcA = nn.Linear(32, self.num_actions)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        V = self.fcV(x)
+        A = self.fcA(x)
+
+        return V.expand(-1, self.num_actions) + (A - A.mean(1).expand(-1, self.num_actions))
