308219d8b59d0b6cca0152e40fb7992e763cbdb4
Yuji Kamiya <y.kamiya0@gmail.com>
2018-09-04 22:54:27 +0900

    introduce priority experience reply

--- python3/reinforcement/cartpole_rainbow.py
+++ python3/reinforcement/cartpole_rainbow.py
@@ -14,6 +14,7 @@ from torch import optim
 import torch.nn.functional as F
 from model import DuelingNetFC
 from config import Config
+from sum_tree import SumTree
 
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
@@ -23,6 +24,7 @@ MAX_STEPS = 200
 NUM_EPISODE = 500
 BATCH_SIZE = 32
 CAPACITY = 10000
+MEMORY_SIZE_TO_START_REPLY = 32
 
 class ReplayMemory:
     def __init__(self, capacity):
@@ -30,20 +32,54 @@ class ReplayMemory:
         self.memory = []
         self.index = 0
 
-    def push(self, state, action, state_next, reward):
+    def push(self, _, transition):
         if len(self.memory) < self.capacity:
             self.memory.append(None)
 
-        self.memory[self.index] = Transition(state, action, state_next, reward)
+        self.memory[self.index] = transition
         self.index = (self.index + 1) % self.capacity
 
     def sample(self, size):
-        return random.sample(self.memory, size)
+        return (None, random.sample(self.memory, size))
+
+    def update(self, idx, td_error):
+        pass
 
     def __len__(self):
         return len(self.memory)
 
+class PERMemory:
+    epsilon = 0.0001
+    alpha = 0.6
+    size = 0
 
+    def __init__(self, capacity):
+        self.tree = SumTree(capacity)
+
+    def _getPriority(self, td_error):
+        return (td_error + self.epsilon) ** self.alpha
+
+    def push(self, td_error, transition):
+        self.size += 1
+        priority = self._getPriority(td_error)
+        self.tree.add(priority, transition)
+
+    def sample(self, size):
+        list = []
+        indexes = []
+        for rand in np.random.uniform(0, self.tree.total(), size):
+            (idx, _, data) = self.tree.get(rand)
+            list.append(data)
+            indexes.append(idx)
+
+        return (indexes, list)
+
+    def update(self, idx, td_error):
+        priority = self._getPriority(td_error)
+        self.tree.update(idx, priority)
+
+    def __len__(self):
+        return self.size
 
 class Brain:
     def __init__(self, config, num_states, num_actions):
@@ -51,7 +87,8 @@ class Brain:
         self.num_states = num_states
         self.num_actions = num_actions
 
-        self.memory = ReplayMemory(CAPACITY)
+        # self.memory = ReplayMemory(CAPACITY)
+        self.memory = PERMemory(CAPACITY)
 
         self.model = DuelingNetFC(num_states, num_actions).to(device=self.config.device)
         self.target_model = DuelingNetFC(num_states, num_actions).to(device=self.config.device)
@@ -59,12 +96,8 @@ class Brain:
         print(self.model)
         self.optimizer = optim.Adam(self.model.parameters(), lr=0.0001)
     
-    def reply(self):
-        if (len(self.memory) < BATCH_SIZE):
-            return
-
-        transitions = self.memory.sample(BATCH_SIZE)
-
+    def _get_state_action_values(self, transitions):
+        batch_size = len(transitions)
         batch = Transition(*zip(*transitions))
 
         non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.uint8, device=self.config.device)
@@ -72,25 +105,50 @@ class Brain:
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
-        non_final_next_state = torch.cat([s for s in batch.next_state if s is not None])
         
         self.model.eval()
 
-        next_state_values = torch.zeros(BATCH_SIZE).to(self.config.device, dtype=torch.float32)
-        best_actions = torch.argmax(self.model(non_final_next_state), dim=1, keepdim=True)
-        next_state_values[non_final_mask] = self.target_model(non_final_next_state).gather(1, best_actions).squeeze()
+        next_state_values = torch.zeros(batch_size).to(self.config.device, dtype=torch.float32)
 
-        expected_state_action_values = reward_batch + GAMMA * next_state_values.detach()
+        next_states = [s for s in batch.next_state if s is not None]
+        if len(next_states) != 0:
+            non_final_next_state = torch.cat(next_states)
+            best_actions = torch.argmax(self.model(non_final_next_state), dim=1, keepdim=True)
+            next_state_values[non_final_mask] = self.target_model(non_final_next_state).gather(1, best_actions).squeeze()
 
-        state_action_values = torch.squeeze(self.model(state_batch).gather(1, action_batch))
-        state_action_values.to(self.config.device, dtype=torch.float32)
-        
+        expected_values = reward_batch + GAMMA * next_state_values.detach()
+
+        values = torch.squeeze(self.model(state_batch).gather(1, action_batch))
+        values.to(self.config.device, dtype=torch.float32)
+
+        return (values, expected_values)
+
+    def reply(self):
+        if (len(self.memory) < MEMORY_SIZE_TO_START_REPLY):
+            return
+
+        indexes, transitions = self.memory.sample(BATCH_SIZE)
+        values, expected_values = self._get_state_action_values(transitions)
+
         self.model.train()
 
-        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
+        loss = F.smooth_l1_loss(values, expected_values)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
+
+        if (indexes != None):
+            for i, value in enumerate(values):
+                td_error = expected_values[i].item() - value.item()
+                self.memory.update(indexes[i], abs(td_error))
+
+    def add_memory(self, transition):
+        values, expected_values = self._get_state_action_values([transition])
+        td_error = abs(values.item() - expected_values.item())
+        self.memory.push(td_error, transition)
+
+        if (len(self.memory) == MEMORY_SIZE_TO_START_REPLY):
+            print('start reply from next step')
 
     def decide_action(self, state, episode):
         epsilon = 0.5 * (1 / (episode + 1))
@@ -115,14 +173,14 @@ class Agent:
         self.steps_accumulated = 0
         self.brain = Brain(config, num_states, num_actions)
 
-    def update_q_function(self):
+    def learn(self):
         self.brain.reply()
 
     def get_action(self, state, step):
         return self.brain.decide_action(state, step)
 
-    def memory(self, state, action, state_next, reward):
-        return self.brain.memory.push(state, action, state_next, reward)
+    def observe(self, state, action, state_next, reward):
+        self.brain.add_memory(Transition(state, action, state_next, reward))
 
     def update_target_model(self):
         self.steps_accumulated += 1
@@ -168,8 +226,8 @@ class Environment:
                 reward = torch.tensor([0.0], dtype=torch.float32, device=self.config.device)
                 state_next = torch.from_numpy(observation_next).to(self.config.device, dtype=torch.float32).unsqueeze(0)
 
-            self.agent.memory(state, action, state_next, reward)
-            self.agent.update_q_function()
+            self.agent.observe(state, action, state_next, reward)
+            self.agent.learn()
             self.agent.update_target_model();
 
             state = state_next
