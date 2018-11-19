ae1509036f1864c773fbcf1662f4523b1a9247a0
Yuji Kamiya <y.kamiya0@gmail.com>
2018-11-20 07:52:44 +0900

    cuda version was upgraded to 9.2 in google colab

--- python3/reinforcement/setup_atari_google_colab.sh
+++ python3/reinforcement/setup_atari_google_colab.sh
@@ -3,20 +3,13 @@
 pip install http://download.pytorch.org/whl/cu92/torch-0.4.1-cp36-cp36m-linux_x86_64.whl
 pip install torchvision
 
-# intall gym-retro
-pip install gym-retro
 pip install gym[atari]
 
 # apt-get update -y
 # apt-get install -y --allow-unauthenticated cmake libopenmpi-dev python3-dev zlib1g-dev
-#
+
 # git clone https://github.com/openai/baselines.git
 # pushd baselines
 # pip install -e .
 # popd
 
-dir=$(dirname $0)
-cp $dir/scenario.json /usr/local/lib/python3.6/dist-packages/retro/data/stable/SonicTheHedgehog-Genesis/
-
-# you should rom.md on current directory in advance
-cp rom.md /usr/local/lib/python3.6/dist-packages/retro/data/stable/SonicTheHedgehog-Genesis/
