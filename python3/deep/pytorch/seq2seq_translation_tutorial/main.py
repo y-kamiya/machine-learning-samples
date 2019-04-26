7c418ef31eacb47de064d19fb4a5d9ceebb1d1b1
Yuji Kamiya <y.kamiya0@gmail.com>
2019-04-27 08:30:05 +0900

    simple attension

--- python3/deep/pytorch/seq2seq_translation_tutorial/main.py
+++ python3/deep/pytorch/seq2seq_translation_tutorial/main.py
@@ -160,7 +160,37 @@ class DecoderRNN(nn.Module):
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
+class AttensionDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=10):
+        super(AttensionDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
 
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.attn = nn.Linear(hidden_size * 2, max_length)
+        self.attn_combine = nn.Linear(hidden_size * 2, hidden_size)
+        self.dropout = nn.Dropout(dropout_p)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden, output):
+        embedded = self.embedding(input).view(1,1,-1)
+        embedded = self.dropout(embedded)
+
+        attn_input = torch.cat([embedded[0], hidden[0]], dim=1)
+        attn_weights = F.softmax(self.attn(attn_input), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0), output.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
 
 def indexesFromSentence(lang, sentence):
     return [lang.word2index[word] for word in sentence.split(' ')]
@@ -304,7 +334,8 @@ def showPlot(points):
 
 hidden_size = 256
 encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
-attn_decoder1 = DecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+# attn_decoder1 = DecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+attn_decoder1 = AttensionDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
 
 trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
 
