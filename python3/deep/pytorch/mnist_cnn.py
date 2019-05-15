99bbd7b7e4c70b5f8f5e1c593d60553f42800543
Yuji Kamiya <y.kamiya0@gmail.com>
2019-05-15 13:24:22 +0900

    fix mnist cnn

--- python3/deep/pytorch/mnist_cnn.py
+++ python3/deep/pytorch/mnist_cnn.py
@@ -2,16 +2,18 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
+import time
+import argparse
+from PIL import Image
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
+from torchvision import datasets, transforms, utils
 
 BATCH_SIZE=32
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class Cnn(nn.Module):
     def __init__(self):
@@ -33,35 +35,60 @@ class Cnn(nn.Module):
         x = self.linear2(x)
         return x
 
-train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=BATCH_SIZE, shuffle=True)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('--epochs', type=int, default=10, help='epoch count')
+    parser.add_argument('--cpu', action='store_true', help='use cpu')
+    parser.add_argument('--model', help='model to load')
+    parser.add_argument('--estimate', help='image file path to estimate class')
+    args = parser.parse_args()
+    print(args)
 
-test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('data', train=False, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=BATCH_SIZE, shuffle=True)
+    is_cpu = args.cpu or not torch.cuda.is_available()
+    device_name = 'cpu' if is_cpu else 'cuda'
+    device = torch.device(device_name)
+
+    model = Cnn().to(device)
+
+    if args.model != None:
+        model.load_state_dict(torch.load(args.model, map_location=device_name), strict=False)
+
+    transform = transform=transforms.Compose([
+        transforms.Grayscale(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    if args.estimate != None:
+        model.eval()
+        input = transform(Image.open(args.estimate)).unsqueeze(0)
+        output = F.softmax(model(input))
+        print('estimated class: {}'.format(np.around(output.data.numpy(), decimals=3)))
+        sys.exit()
+
+    train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('data', train=True, download=True, transform=transform),
+            batch_size=BATCH_SIZE, shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('data', train=False, download=True, transform=transform),
+            batch_size=BATCH_SIZE, shuffle=True)
 
 
-model = Cnn().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-optimizer = optim.SGD(model.parameters(), lr=0.01)
+    start = time.time()
+    for i in range(args.epochs):
+        model.train()
+        for batch_index, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.cross_entropy(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_index % 100 == 0:
+                print("elapsed: {:0.2f} sec, step {}, loss {:0.3f}".format(time.time() - start, batch_index * len(data), loss.item()))
 
-for i in range(3):
-    model.train()
-    for batch_index, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.cross_entropy(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_index % 100 == 0:
-            print("step %d, loss %g" % (batch_index * len(data), loss.item()))
+        torch.save(model.state_dict(), 'data/mnist_cnn_{}.model'.format(i))
+
