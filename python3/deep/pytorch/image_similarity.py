af895508e08a8a147d54d476e8c80a628c367710
Yuji Kamiya <y.kamiya0@gmail.com>
2019-10-23 19:00:07 +0900

    refactor

--- python3/deep/pytorch/image_similarity.py
+++ python3/deep/pytorch/image_similarity.py
@@ -13,43 +13,6 @@ from torchvision.utils import save_image
 from PIL import Image
 import tabulate
 
-parser = argparse.ArgumentParser(description='VAE MNIST Example')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--model', default=None,
-                    help='model path to load')
-parser.add_argument('--dataroot', default='./data',
-                    help='where the data directory exists')
-parser.add_argument('--latent-feature', default='',
-                    help='image file path to get latent feature')
-parser.add_argument('--analyze', action='store_true',
-                    help='compare cosine similarity of images')
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-
-torch.manual_seed(args.seed)
-
-device_name = "cuda" if args.cuda else "cpu"
-device = torch.device(device_name)
-
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(args.dataroot, train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(args.dataroot, train=False, transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-
 class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
@@ -82,140 +45,186 @@ class VAE(nn.Module):
         mu, logvar = self.encode(x.view(-1, 784))
         return self.reparameterize(mu, logvar)
 
-model = VAE().to(device)
-if args.model != None:
-    model.load_state_dict(torch.load(args.model, map_location=device_name), strict=False)
+class Trainer():
+    def __init__(self, config):
+        self.config = config
 
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+        self.model = VAE().to(config.device)
+        if config.model != None:
+            self.model.load_state_dict(torch.load(config.model, map_location=config.device_name), strict=False)
 
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
 
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+        kwargs = {'num_workers': 1, 'pin_memory': True} if config.cuda else {}
 
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        self.train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST(config.dataroot, train=True, download=True,
+                           transform=transforms.ToTensor()),
+            batch_size=config.batch_size, shuffle=True, **kwargs)
 
-    return BCE + KLD
+        self.test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST(config.dataroot, train=False, transform=transforms.ToTensor()),
+            batch_size=config.batch_size, shuffle=True, **kwargs)
 
+    # Reconstruction + KL divergence losses summed over all elements and batch
+    def __loss_function(self, recon_x, x, mu, logvar):
+        BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
 
-def train(epoch):
-    model.train()
-    train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
-        data = data.to(device)
-        optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
-
-    model_dir = '{}/model'.format(args.output_dir)
-    os.makedirs(model_dir, exist_ok=True)
-    torch.save(model.state_dict(), '{}/epoch{}.pth'.format(model_dir, epoch))
+        return BCE + KLD
 
 
-def test(epoch):
-    model.eval()
-    test_loss = 0
-    with torch.no_grad():
-        for i, (data, _) in enumerate(test_loader):
-            data = data.to(device)
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
-            if i == 0:
-                n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n],
-                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-                output_file = '{}/reconstruction_{}.png'.format(args.output_dir, str(epoch))
-                save_image(comparison.cpu(), output_file, nrow=n)
+    def train(self, epoch):
+        self.model.train()
+        train_loss = 0
+        for batch_idx, (data, _) in enumerate(self.train_loader):
+            data = data.to(self.config.device)
+            self.optimizer.zero_grad()
+            recon_batch, mu, logvar = self.model(data)
+            loss = self.__loss_function(recon_batch, data, mu, logvar)
+            loss.backward()
+            train_loss += loss.item()
+            self.optimizer.step()
+            if batch_idx % self.config.log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(self.train_loader.dataset),
+                    100. * batch_idx / len(self.train_loader),
+                    loss.item() / len(data)))
 
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
+        print('====> Epoch: {} Average loss: {:.4f}'.format(
+              epoch, train_loss / len(self.train_loader.dataset)))
 
-def latent_feature():
-    model.eval()
-    with torch.no_grad():
-        pickle_path = '{}/latent_feature.pickle'.format(args.output_dir)
-        data = []
+        model_dir = '{}/model'.format(self.config.output_dir)
+        os.makedirs(model_dir, exist_ok=True)
+        torch.save(self.model.state_dict(), '{}/epoch{}.pth'.format(model_dir, epoch))
+
+
+    def test(self, epoch):
+        self.model.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for i, (data, _) in enumerate(self.test_loader):
+                data = data.to(self.config.device)
+                recon_batch, mu, logvar = self.model(data)
+                test_loss += self.__loss_function(recon_batch, data, mu, logvar).item()
+                if i == 0:
+                    n = min(data.size(0), 8)
+                    comparison = torch.cat([data[:n],
+                                          recon_batch.view(self.config.batch_size, 1, 28, 28)[:n]])
+                    output_file = '{}/reconstruction_{}.png'.format(self.config.output_dir, str(epoch))
+                    save_image(comparison.cpu(), output_file, nrow=n)
+
+        test_loss /= len(self.test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_loss))
+
+    def latent_feature(self):
+        self.model.eval()
+        with torch.no_grad():
+            pickle_path = '{}/latent_feature.pickle'.format(self.config.output_dir)
+            data = []
+            if os.path.exists(pickle_path):
+                with open(pickle_path, 'rb') as fp:
+                    data = pickle.load(fp)
+
+            filename = self.config.latent_feature.split('/')[-1]
+            label = filename.split('.')[0]
+
+            image = Image.open('{}/{}'.format(self.config.dataroot, self.config.latent_feature))
+            x = transforms.functional.to_tensor(image).unsqueeze(0).to(self.config.device)
+
+            z = self.model.latent_feature(x).squeeze()
+            data.append({
+                'path': self.config.latent_feature,
+                'label': label,
+                'feature': z,
+            })
+
+            with open(pickle_path, 'wb') as fp:
+                pickle.dump(data, fp)
+
+    def __cos_sim(self, v1, v2):
+        return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+    def __img_tag(self, src):
+        path = os.path.abspath('{}/{}'.format(self.config.dataroot, src))
+        return '<img src="{}" width="50" height="50">'.format(path)
+
+    def analyze(self):
+        pickle_path = '{}/latent_feature.pickle'.format(self.config.output_dir)
         if os.path.exists(pickle_path):
             with open(pickle_path, 'rb') as fp:
                 data = pickle.load(fp)
 
-        filename = args.latent_feature.split('/')[-1]
-        label = filename.split('.')[0]
+        table = []
+        for row in data:
+            d = []
+            for col in data:
+                d.append(self.__cos_sim(row['feature'].numpy(), col['feature'].numpy()))
+            table.append(d)
 
-        image = Image.open('{}/{}'.format(args.dataroot, args.latent_feature))
-        x = transforms.functional.to_tensor(image).unsqueeze(0).to(device)
+        for i in range(len(data)):
+            table[i].insert(0, self.__img_tag(data[i]['path']))
 
-        z = model.latent_feature(x).squeeze()
-        data.append({
-            'path': args.latent_feature,
-            'label': label,
-            'feature': z,
-        })
+        headers = [self.__img_tag(entry['path']) for entry in data]
+        headers.insert(0, "")
 
-        with open(pickle_path, 'wb') as fp:
-            pickle.dump(data, fp)
+        html = tabulate.tabulate(table, headers, tablefmt='html', floatfmt='.3f', numalign='right')
+        print(html)
 
-def cos_sim(v1, v2):
-    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-
-def img_tag(src):
-    path = os.path.abspath('{}/{}'.format(args.dataroot, src))
-    return '<img src="{}" width="50" height="50">'.format(path)
-
-def analyze():
-    pickle_path = '{}/latent_feature.pickle'.format(args.output_dir)
-    if os.path.exists(pickle_path):
-        with open(pickle_path, 'rb') as fp:
-            data = pickle.load(fp)
-
-    table = []
-    for row in data:
-        d = []
-        for col in data:
-            d.append(cos_sim(row['feature'].numpy(), col['feature'].numpy()))
-        table.append(d)
-
-    for i in range(len(data)):
-        table[i].insert(0, img_tag(data[i]['path']))
-
-    headers = [img_tag(entry['path']) for entry in data]
-    headers.insert(0, "")
-
-    html = tabulate.tabulate(table, headers, tablefmt='html', floatfmt='.3f', numalign='right')
-    print(html)
+    def sample_image(self):
+        with torch.no_grad():
+            sample = torch.randn(64, 20).to(self.config.device)
+            sample = self.model.decode(sample).cpu()
+            output_file = '{}/sample_{}.png'.format(self.config.output_dir, str(epoch))
+            save_image(sample.view(64, 1, 28, 28), output_file)
 
 if __name__ == "__main__":
-    args.output_dir = '{}/vae'.format(args.dataroot)
+    parser = argparse.ArgumentParser(description='check image similarity')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--model', default=None,
+                        help='model path to load')
+    parser.add_argument('--dataroot', default='./data',
+                        help='where the data directory exists')
+    parser.add_argument('--latent-feature', default='',
+                        help='image file path to get latent feature')
+    parser.add_argument('--analyze', action='store_true',
+                        help='compare cosine similarity of images')
+    args = parser.parse_args()
+
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    args.output_dir = '{}/image_similarity'.format(args.dataroot)
+    args.device_name = "cuda" if args.cuda else "cpu"
+    args.device = torch.device(args.device_name)
+
     os.makedirs(args.output_dir, exist_ok=True)
 
+    torch.manual_seed(args.seed)
+
+    trainer = Trainer(args)
+
     if args.latent_feature:
-        latent_feature()
+        trainer.latent_feature()
         sys.exit()
 
     if args.analyze:
-        analyze()
+        trainer.analyze()
         sys.exit()
 
     for epoch in range(1, args.epochs + 1):
-        train(epoch)
-        test(epoch)
-        with torch.no_grad():
-            sample = torch.randn(64, 20).to(device)
-            sample = model.decode(sample).cpu()
-            output_file = '{}/sample_{}.png'.format(args.output_dir, str(epoch))
-            save_image(sample.view(64, 1, 28, 28), output_file)
+        trainer.train(epoch)
+        trainer.test(epoch)
+        trainer.sample_image()
