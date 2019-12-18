5d8a0354ed583e82a1f62ea5f6c77041044f1cf6
Yuji Kamiya <y.kamiya0@gmail.com>
2019-12-18 13:06:20 +0900

    extract node image

--- python3/deep/pytorch/image_similarity/preprocess_captures.py
+++ python3/deep/pytorch/image_similarity/preprocess_captures.py
@@ -3,6 +3,10 @@ import sys
 import glob
 import shutil
 import argparse
+import csv
+import cv2
+import hashlib
+from tqdm import tqdm
 
 def extract_label():
     for file in os.listdir(args.target_dir):
@@ -24,12 +28,90 @@ def classify_with_label():
         if count < args.count:
             shutil.copyfile(path, '{}/{}'.format(label_dir, file))
 
+def create_csv():
+    output_file = '{}/labels.csv'.format(args.target_dir)
+    target_dir_abs = os.path.realpath(args.target_dir)
+    data = []
+    for file in os.listdir(args.target_dir):
+        labels = file.rsplit('_', 1)[0]
+        labels = labels.replace('@', ',')
+        path = '{}/{}'.format(target_dir_abs, file)
+        data.append('{},{}'.format(path, labels))
+    
+    with open(output_file, mode='w') as f:
+        f.write('\n'.join(data))
+
+def extract_node_image():
+    os.makedirs('{}/node'.format(args.target_dir), exist_ok=True)
+    os.makedirs('{}/processed'.format(args.target_dir), exist_ok=True)
+
+    images = {}
+    for file in os.listdir(args.target_dir):
+        splited = os.path.splitext(file)
+        if splited[1] != '.png':
+            continue
+        hash = splited[0].split('_')[1]
+        images[hash] = file
+
+    node_data_file = '{}/node_uniq.csv'.format(args.target_dir)
+    with open(node_data_file) as f:
+        reader = csv.reader(f)
+        current_hash = ''
+        for entry in tqdm(reader):
+            hash = entry[0]
+            if hash not in images:
+                continue
+
+            if hash != current_hash:
+                image = None
+                current_hash = hash
+                path = '{}/{}'.format(args.target_dir, images[hash])
+                if os.path.exists(path):
+                    image = cv2.imread(path)
+                    dir = '{}/processed/'.format(args.target_dir)
+                    shutil.move(path, dir)
+
+            if image is None:
+                continue
+
+            x = max(int(entry[1]), 0)
+            y = max(int(entry[2]), 0)
+            w = int(entry[3])
+            h = int(entry[4])
+            label = entry[5].split('@')[0]
+
+            height, width, _ = image.shape
+            cv_y = height - y
+            save_image(image[cv_y-h:cv_y, x:x+w], label, 'raw')
+            # save_image(image[cv_y-h:cv_y-h//2, x:x+w//2], label)
+            # save_image(image[cv_y-h:cv_y-h//2, x+w//2:x+w], label)
+            # save_image(image[cv_y-h//2:cv_y, x:x+w//2], label)
+            # save_image(image[cv_y-h//2:cv_y, x+w//2:x+w], label)
+            # save_image(image[cv_y-h*3//4:cv_y-h//4, x+w//4:x+w*3//4], label)
+            if h < 240 and w < 420:
+                save_image(image[cv_y-h-12:cv_y+12, x-21:x+w+21], label, 'wide')
+            if 800 < w:
+                save_image(image[cv_y-h:cv_y, x:x+w//2], label, 'crop')
+                save_image(image[cv_y-h:cv_y, x+w//2:x+w], label, 'crop')
+            if 600 < h:
+                save_image(image[cv_y-h:cv_y-h//2, x:x+w], label, 'crop')
+                save_image(image[cv_y-h//2:cv_y-h, x:x+w], label, 'crop')
+            if 800 < w and 600 < h:
+                save_image(image[cv_y-h*3//4:cv_y-h//4, x+w//4:x+w*3//4], label, 'crop')
+
+def save_image(image, label, dir):
+    hash = hashlib.md5(image.tobytes()).hexdigest()
+    path = '{}/node/{}/{}_{}.jpg'.format(args.target_dir, dir, label, hash)
+    cv2.imwrite(path, image)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='check image similarity')
     parser.add_argument('target_dir', help='target image directory path')
     parser.add_argument('--extract_label', action='store_true', help='extract label')
+    parser.add_argument('--extract_node_image', action='store_true', help='extract image from screenshot')
     parser.add_argument('--classify_with_label', action='store_true', help='classify images with label in file name')
+    parser.add_argument('--csv', action='store_true', help='create csv file including map between images and labels')
     parser.add_argument('--count', type=int, default=10, help='file count to process')
     parser.add_argument('--target_label', default=None, help='label to process')
     parser.add_argument('--dest_dir', default=None, help='destination directory')
@@ -40,6 +122,15 @@ if __name__ == "__main__":
         extract_label()
         sys.exit()
 
+    if args.extract_node_image:
+        extract_node_image()
+        sys.exit()
+
     if args.classify_with_label:
         classify_with_label()
         sys.exit()
+
+    if args.csv:
+        create_csv()
+        sys.exit()
+
