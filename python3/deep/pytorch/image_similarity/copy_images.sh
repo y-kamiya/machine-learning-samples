9dbc9b40e9bdfcd4cd8ab88a8fb2cefa51b7a48e
Yuji Kamiya <y.kamiya0@gmail.com>
2019-12-31 23:27:38 +0900

    fix copy images

--- python3/deep/pytorch/image_similarity/copy_images.sh
+++ python3/deep/pytorch/image_similarity/copy_images.sh
@@ -5,7 +5,6 @@ from=$2
 to=$3
 count=$4
 
-for classname in $(cat $classlist);
-do
-    find $from -name "$classname@*" | shuf | head -n $count | xargs -I@ cp @ $to
-done
+cat $classlist | xargs -I{} bash -c "mkdir -p $to/{}"
+cat $classlist | gxargs -n1 -I{} -P8 bash -c "find $from -maxdepth 2 -name \"{}@*\" | shuf | head -n $count | gxargs -I@ -P8 cp @ $to/{}/"
+#head -n $count | gxargs -I@ -P8 echo @ $to/{}"
