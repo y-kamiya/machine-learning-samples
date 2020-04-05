5d527d3e7034a3379ed3394cc640535ef2b3e05a
Yuji Kamiya <y.kamiya0@gmail.com>
2020-04-05 14:47:37 +0900

    fix mask to avoid nan

--- python3/deep/pytorch/transformer/transformer.py
+++ python3/deep/pytorch/transformer/transformer.py
@@ -161,8 +161,7 @@ class TransformerModel(nn.Module):
         source_mask_np = np.triu(np.ones(shape), k=1).astype('uint8')
         source_mask = torch.from_numpy(source_mask_np) == 1
         pad_mask = mask.unsqueeze(-2)
-        pad_mask_t = torch.transpose(pad_mask, 1, 2)
-        source_mask = pad_mask | pad_mask_t | source_mask
+        source_mask = pad_mask | source_mask
 
         return mask.to(device), source_mask.to(device)
 
