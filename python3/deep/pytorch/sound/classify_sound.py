3517923bb634622da4bbab7476590c8868d69e85
Yuji Kamiya <y.kamiya0@gmail.com>
2020-06-01 00:05:44 +0900

    add log

--- python3/deep/pytorch/sound/classify_sound.py
+++ python3/deep/pytorch/sound/classify_sound.py
@@ -4,11 +4,13 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
 import torchaudio
 import pandas as pd
 import numpy as np
 import os
 import argparse
+import time
 
 class Trainer():
     def __init__(self, config):
@@ -17,6 +19,7 @@ class Trainer():
         self.model = Model_M5().to(config.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr = 0.01, weight_decay = 0.0001)
         self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size = 20, gamma = 0.1)
+        self.writer = SummaryWriter(log_dir=config.tensorboard_log_dir)
 
     def update_epoch(self):
         self.scheduler.step()
@@ -38,9 +41,12 @@ class Trainer():
             loss.backward()
             self.optimizer.step()
 
+            n_processed_data = epoch * (batch_idx+1) * self.config.batch_size
+            self.writer.add_scalar('loss/train', loss, n_processed_data, time.time())
+
             if batch_idx % self.config.log_interval == 0: #print training stats
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    epoch, batch_idx * self.config.batch_size, len(train_loader.dataset),
                     100. * batch_idx / len(train_loader), loss))
 
     def eval(self, dataloader, epoch):
@@ -48,6 +54,7 @@ class Trainer():
         device = self.config.device
 
         correct = 0
+        total_loss = 0
         for data, target in dataloader:
             data = data.to(device)
             target = target.to(device)
@@ -55,10 +62,14 @@ class Trainer():
             output = output.permute(1, 0, 2)
             pred = output.max(2)[1]
             correct += pred.eq(target).cpu().sum().item()
+            total_loss += F.nll_loss(output[0], target, reduction='sum')
+
+        n_data = len(dataloader.dataset)
+        self.writer.add_scalar('loss/eval', total_loss / n_data, epoch, time.time())
+        self.writer.add_scalar('loss/acc', correct, epoch, time.time())
 
         print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
-            correct, len(dataloader.dataset),
-            100. * correct / len(dataloader.dataset)))
+            correct, n_data, 100. * correct / n_data))
 
 class Model_M5(nn.Module):
     def __init__(self):
@@ -133,6 +144,8 @@ class SoundDataset(Dataset):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('--cpu', action='store_true', help='use cpu')
+    parser.add_argument('--dataroot', default='data', help='path to data')
+    parser.add_argument('--name', default='default', help='name of training, used to model name, log dir name etc')
     parser.add_argument('--batch_size', type=int, default=8, help='epoch count')
     parser.add_argument('--log_interval', type=int, default=1, help='log interval epochs')
     parser.add_argument('--epochs', type=int, default=40, help='epoch count')
@@ -143,9 +156,11 @@ if __name__ == '__main__':
     args.device_name = "cpu" if is_cpu else "cuda"
     args.device = torch.device(args.device_name)
 
-    data_dir = 'data/ESC-50-master'
-    csv_path = f'{data_dir}/meta/esc50.csv'
-    audio_dir = f'{data_dir}/audio'
+    args.tensorboard_log_dir = f'{args.dataroot}/runs/{args.name}'
+    os.makedirs(args.tensorboard_log_dir, exist_ok=True)
+
+    csv_path = f'{args.dataroot}/meta/esc50.csv'
+    audio_dir = f'{args.dataroot}/audio'
     train_dataset = SoundDataset(csv_path, audio_dir, range(1, 5))
     eval_dataset = SoundDataset(csv_path, audio_dir, [5])
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
