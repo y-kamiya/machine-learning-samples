c5ec4028afb8894d0472de3a6052542553537450
Yuji Kamiya <y.kamiya0@gmail.com>
2020-06-13 09:21:15 +0900

    create dataset with adjust per class

--- tools/object-detection/create_dataset.sh
+++ tools/object-detection/create_dataset.sh
@@ -13,23 +13,25 @@ python $script_dir/augmentator.py --rotate $SOURCE_DIR/resize
 rm -rf $all_dir
 mkdir -p $all_dir
 
-count_resize=$(ls $SOURCE_DIR/resize | wc -l)
+count_resize=$(find $SOURCE_DIR/resize -maxdepth 1 -name '*.xml' | wc -l)
 count_rotate=$((count_resize / 3))
 echo $count_resize
 echo $count_rotate
 echo =============
-find $SOURCE_DIR/resize/rotate -maxdepth 1 -name '*.jpg' \
-| sed 's/\.jpg//' | shuf | head -n $count_rotate | xargs -n1 -I@ mv @.jpg @.xml $all_dir
+find $SOURCE_DIR/resize/rotate -maxdepth 1 -name '*.xml' \
+| sed 's/\.xml//' | shuf | head -n $count_rotate | xargs -n1 -I@ mv @.jpg @.xml $all_dir
 
 # mv $SOURCE_DIR/resize/rotate $all_dir
-mv $SOURCE_DIR/resize/*.jpg $all_dir
-mv $SOURCE_DIR/resize/*.xml $all_dir
+find $SOURCE_DIR/resize/ -maxdepth 1 -name '*.jpg' | xargs -I@ mv @ $all_dir
+find $SOURCE_DIR/resize/ -maxdepth 1 -name '*.xml' | xargs -I@ mv @ $all_dir
 
 pushd $all_dir
 
 mkdir -p train
-find . -maxdepth 1 -name '*.jpg' \
-| sed 's/\.jpg//' | shuf | head -n $COUNT | xargs -n1 -I@ mv @.jpg @.xml train
+python $script_dir/annotator.py ./ --copy_images_per_class $COUNT --output_dir ./train --mv
+
+# find . -maxdepth 1 -name '*.xml' \
+# | sed 's/\.xml//' | shuf | head -n $COUNT | xargs -n1 -I@ mv @.jpg @.xml train
 
 popd
 
