cb3c0bcc20f72abe4506875cccd7b130e582df1c
Yuji Kamiya <y.kamiya0@gmail.com>
2020-09-02 21:01:47 +0900

    similar words with sampling vector

--- python3/deep/pytorch/classify-words/word_embeddings.py
+++ python3/deep/pytorch/classify-words/word_embeddings.py
@@ -1,9 +1,12 @@
 import sys
 import argparse
+import heapq
+import random
+import numpy as np
 from logzero import setup_logger
 from wikipedia2vec import Wikipedia2Vec
 
-class WordEmbeddings():
+class SimilarWordDetector():
     CATEGORIES = ['ライブ','ポップ','カルチャー','社会','科学','技術','総合', '知識']
 
     def __init__(self, pkl_path, config):
@@ -13,27 +16,70 @@ class WordEmbeddings():
     def execute(self):
         for word in self.CATEGORIES:
             print('original word: {}'.format(word))
-            similar_words = self.model.most_similar(self.model.get_word(word), 200)
+            similar_words = self.similar_words(word)
             for entry in similar_words:
                 print('{}\t{}'.format(entry[0], entry[1]))
 
-
-
+    def test(self):
+        print(self.model.dictionary.__dict__)
         # words = self.model.dictionary.words()
         # for i, word in enumerate(words):
         #     if i == 10:
         #         break
         #     print(word)
 
+
+    def similar_words(self, original_word):
+        model_dim = self.model.train_params['dim_size']
+        n_top = self.config.n_top
+        n_dim = self.config.n_dim
+        if model_dim <= n_dim:
+            return self.model.most_similar(self.model.get_word(original_word), n_top)
+
+        sampling_dims = random.sample(range(0, model_dim), n_dim)
+        original_vector = self.model.get_word_vector(original_word)
+        original_vector = self.filter_vector(original_vector, sampling_dims)
+
+        queue = []
+        for word in self.model.dictionary.words():
+            vector = self.model.get_word_vector(word.text)
+            vector = self.filter_vector(vector, sampling_dims)
+            similarity = self.cos_sim(original_vector, vector)
+            heapq.heappush(queue, (-similarity, word))
+
+        words = []
+        for i in range(n_top):
+            similarity, word = heapq.heappop(queue)
+            words.append((word, -similarity))
+        
+        return words
+        
+    def filter_vector(self, vector, indexes):
+        filter = np.zeros(self.model.train_params['dim_size'])
+        for index in indexes:
+            filter[index] = 1
+
+        return vector * filter
+
+    def cos_sim(self, v1, v2):
+        return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('pkl_path', help='pretrained pkl file path')
     parser.add_argument('--loglevel', default='DEBUG')
+    parser.add_argument('--n_dim', type=int, default=300, help='dimention to calculate similarity')
+    parser.add_argument('--n_top', type=int, default=10, help='number of output words')
+    parser.add_argument('--test', action='store_true')
     args = parser.parse_args()
 
     logger = setup_logger(name=__name__, level=args.loglevel)
     logger.info(args)
     args.logger = logger
 
-    word_embeddings = WordEmbeddings(args.pkl_path, args)
-    word_embeddings.execute()
+    detector = SimilarWordDetector(args.pkl_path, args)
+    if args.test:
+        detector.test()
+        sys.exit()
+
+    detector.execute()
