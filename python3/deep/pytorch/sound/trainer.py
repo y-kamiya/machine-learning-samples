d0e047a2a99063cad7c72b31bd6152b5312cacfe
Yuji Kamiya <y.kamiya0@gmail.com>
2020-10-10 11:52:50 +0900

    eval batch size

--- python3/deep/pytorch/sound/trainer.py
+++ python3/deep/pytorch/sound/trainer.py
@@ -161,7 +161,7 @@ def train(args, train_folds, eval_folds):
         eval_dataset = dataset.WaveDataset(args, csv_path, audio_dir, eval_folds)
 
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-    eval_loader = DataLoader(eval_dataset, batch_size=args.batch_size, shuffle=False)
+    eval_loader = DataLoader(eval_dataset, batch_size=1, shuffle=False)
 
     accuracy = 0.0
     for epoch in range(1, args.epochs + 1):
