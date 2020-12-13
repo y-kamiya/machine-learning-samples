661cfa8483c4bb7dafc71a6231df344873183d57
Yuji Kamiya <y.kamiya0@gmail.com>
2020-12-13 21:18:36 +0900

    sample using tpu with pytorch

--- python3/deep/pytorch/mnist_cnn_tpu.py
+++ python3/deep/pytorch/mnist_cnn_tpu.py
@@ -5,15 +5,22 @@ from __future__ import print_function
 import sys
 import time
 import argparse
-from PIL import Image
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.debug.metrics as met
+import torch_xla.distributed.parallel_loader as pl
+import torch_xla.distributed.xla_multiprocessing as xmp
 from torchvision import datasets, transforms, utils
 
-BATCH_SIZE=32
+BATCH_SIZE = 32
+LEARNING_RATE = 0.01
+N_WORKERS = 4
+N_CORES = 8
+
 
 class Cnn(nn.Module):
     def __init__(self):
@@ -35,60 +42,130 @@ class Cnn(nn.Module):
         x = self.linear2(x)
         return x
 
+
+class Trainer():
+    def __init__(self, config):
+        self.config = config
+
+    def get_dataset(self):
+        transform = transforms.Compose([
+            transforms.Grayscale(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+
+        train_dataset = datasets.MNIST(self.config.dataroot, train=True, download=True, transform=transform)
+        test_dataset = datasets.MNIST(self.config.dataroot, train=False, download=True, transform=transform)
+
+        return train_dataset, test_dataset
+
+
+    def train(self):
+        train_dataset, test_dataset = SERIAL_EXECUTOR.run(self.get_dataset)
+
+        train_sampler = torch.utils.data.distributed.DistributedSampler(
+            train_dataset,
+            num_replicas=xm.xrt_world_size(),
+            rank=xm.get_ordinal(),
+            shuffle=True)
+
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset,
+            batch_size=BATCH_SIZE,
+            sampler=train_sampler,
+            num_workers=self.config.n_workers,
+            drop_last=True)
+
+        test_loader = torch.utils.data.DataLoader(
+            test_dataset,
+            batch_size=BATCH_SIZE,
+            shuffle=False,
+            num_workers=self.config.n_workers,
+            drop_last=True)
+
+        lr = LEARNING_RATE * xm.xrt_world_size()
+
+        device = xm.xla_device()
+        model = WRAPPED_MODEL.to(device)
+        optimizer = optim.SGD(model.parameters(), lr=lr)
+        loss_fn = nn.NLLLoss()
+
+        # if args.model != None:
+        #     model.load_state_dict(torch.load(args.model, map_location=device_name), strict=False)
+
+        start_time = time.time()
+
+        def train_loop_fn(loader):
+            tracker = xm.RateTracker()
+            model.train()
+
+            for batch_index, (data, target) in enumerate(loader):
+                optimizer.zero_grad()
+                output = model(data)
+                loss = loss_fn(output, target)
+                loss.backward()
+                xm.optimizer_step(optimizer)
+
+                tracker.add(BATCH_SIZE)
+                if batch_index % 100 == 0:
+                    elapsed_time = time.time() - start_time
+                    print("[xla:{}]({}) Elapsed: {:0.2f} sec, Loss: {:0.3f}, Rate: {:.2f}, GlobalRate: {:.2f}, AscTime: {}".format(
+                        xm.get_ordinal(), batch_index, elapsed_time, loss.item(),
+                        tracker.rate(), tracker.global_rate(), time.asctime()), flush=True)
+
+        def test_loop_fn(loader):
+            total_samples = 0
+            correct = 0
+            model.eval()
+            data, pred, target = None, None, None
+            for data, target in loader:
+                output = model(data)
+                pred = output.max(1, keepdim=True)[1]
+                correct += pred.eq(target.view_as(pred)).sum().item()
+                total_samples += data.size()[0]
+
+            accuracy = 100.0 * correct / total_samples
+            print('[xla:{}] Accuracy={:.2f}%'.format(
+                xm.get_ordinal(), accuracy), flush=True)
+            return accuracy, data, pred, target
+
+        for epoch in range(1, args.epochs + 1):
+            para_loader = pl.ParallelLoader(train_loader, [device])
+            train_loop_fn(para_loader.per_device_loader(device))
+            xm.master_print('Finished training epoch {}'.format(epoch))
+
+            para_loader = pl.ParallelLoader(test_loader, [device])
+            accuracy, data, pred, target = test_loop_fn(para_loader.per_device_loader(device))
+
+            if self.config.metrics_debug:
+                xm.master_print(met.metrics_report(), flush=True)
+
+        return accuracy, data, pred, target
+
+
+def mp_fn(rank, config):
+    torch.set_default_tensor_type('torch.FloatTensor')
+    trainer = Trainer(config)
+    trainer.train()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('--epochs', type=int, default=10, help='epoch count')
     parser.add_argument('--cpu', action='store_true', help='use cpu')
+    parser.add_argument('--dataroot', default='data')
     parser.add_argument('--model', help='model to load')
     parser.add_argument('--estimate', help='image file path to estimate class')
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--n_workers', type=int, default=4)
+    parser.add_argument('--n_cores', type=int, default=8)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--log_steps', type=int, default=20)
+    parser.add_argument('--metrics_debug', action='store_true')
     args = parser.parse_args()
     print(args)
 
-    is_cpu = args.cpu or not torch.cuda.is_available()
-    device_name = 'cpu' if is_cpu else 'cuda'
-    device = torch.device(device_name)
+    SERIAL_EXECUTOR = xmp.MpSerialExecutor()
+    WRAPPED_MODEL = xmp.MpModelWrapper(Cnn())
 
-    model = Cnn().to(device)
-
-    if args.model != None:
-        model.load_state_dict(torch.load(args.model, map_location=device_name), strict=False)
-
-    transform = transform=transforms.Compose([
-        transforms.Grayscale(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])
-
-    if args.estimate != None:
-        model.eval()
-        input = transform(Image.open(args.estimate)).unsqueeze(0)
-        output = F.softmax(model(input))
-        print('estimated class: {}'.format(np.around(output.data.numpy(), decimals=3)))
-        sys.exit()
-
-    train_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('data', train=True, download=True, transform=transform),
-            batch_size=BATCH_SIZE, shuffle=True)
-
-    test_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('data', train=False, download=True, transform=transform),
-            batch_size=BATCH_SIZE, shuffle=True)
-
-
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
-
-    start = time.time()
-    for i in range(args.epochs):
-        model.train()
-        for batch_index, (data, target) in enumerate(train_loader):
-            data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.cross_entropy(output, target)
-            loss.backward()
-            optimizer.step()
-            if batch_index % 100 == 0:
-                print("elapsed: {:0.2f} sec, step {}, loss {:0.3f}".format(time.time() - start, batch_index * len(data), loss.item()))
-
-        torch.save(model.state_dict(), 'data/mnist_cnn_{}.model'.format(i))
-
+    xmp.spawn(mp_fn, args=(args,), nprocs=args.n_cores, start_method='fork')
