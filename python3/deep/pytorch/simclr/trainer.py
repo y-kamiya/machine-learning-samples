e225140edc4acb5b413b1a9e752bd305a555e8f4
Yuji Kamiya <y.kamiya0@gmail.com>
2021-02-14 13:08:37 +0900

    update trainer.py

--- python3/deep/pytorch/simclr/trainer.py
+++ python3/deep/pytorch/simclr/trainer.py
@@ -135,32 +135,59 @@ class Trainer:
     def __init__(self, config):
         self.config = config
 
-        self.model = Model(config)
-        self.eval_trainer = LinearEvaluationTrainer(config, self.model)
+        self.device = config.device
+        self.model = Model(config).to(self.device)
 
         lr = 0.075 * math.sqrt(config.batch_size)
         self.optimizer = optim.SGD(self.model.parameters(), lr=lr, weight_decay=1e-6)
 
-        self.dataloader_train = DataLoader(SimCLRDataset(config, 'train'), batch_size=config.batch_size, shuffle=True)
+        self.lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(
+            self.optimizer, config.epochs - config.warmup_epochs)
+
+        self.warmup_scheduler = optim.lr_scheduler.LambdaLR(
+            self.optimizer, lr_lambda=lambda epoch: min(1.0, (epoch + 1) / config.warmup_epochs))
+
+        train_dataset = SimCLRDataset(config, 'train')
+
+        self.dataloader_train = torch.utils.data.DataLoader(
+            train_dataset,
+            batch_size=self.config.batch_size,
+            drop_last=True)
 
         self.writer = SummaryWriter(log_dir=config.tensorboard_log_dir)
+        self.start_time = time.time()
 
-    def train(self, epoch):
+        self.start_epoch = self.load(config.model_path)
+
+    def train(self):
+        for epoch in range(self.start_epoch, args.epochs + 1):
+            self.train_loop_fn(self.dataloader_train, epoch)
+
+            if epoch % self.config.save_interval == 0:
+                self.save(self.config.model_path, epoch)
+
+            if self.config.warmup_epochs < epoch:
+                self.lr_scheduler.step()
+            else:
+                self.warmup_scheduler.step()
+
+            self.config.logger.info('Finished training epoch {}'.format(epoch))
+
+    def train_loop_fn(self, loader, epoch):
         self.model.train()
 
-        for i, (x_odd, x_even) in enumerate(self.dataloader_train):
-            start_time = time.time()
+        for i, (x_odd, x_even) in enumerate(loader):
             self.optimizer.zero_grad()
             loss = self.__loss(x_odd, x_even)
             loss.backward()
-
             self.optimizer.step()
 
             if i % self.config.log_interval == 0:
-                elapsed_time = time.time() - start_time
-                self.config.logger.info('train epoch: {}, step: {}, loss: {:.2f}, time: {:.2f}'.format(epoch, i, loss, elapsed_time))
+                elapsed_time = time.time() - self.start_time
+                self.config.logger.info("train epoch: {}, step: {}, Elapsed: {:0.2f} sec, Loss: {:0.3f}, LR: {:.2e} {:.2e}".format(
+                    epoch, i, elapsed_time, loss.item(), self.warmup_scheduler.get_last_lr()[0], self.lr_scheduler.get_last_lr()[0]))
 
-            self.writer.add_scalar('train/loss', loss, epoch, start_time)
+            self.writer.add_scalar('train/loss', loss, epoch, time.time())
 
     def __loss(self, x_odd, x_even):
         z_odd, h_odd = self.model(x_odd)
@@ -177,43 +204,84 @@ class Trainer:
     def __similarity(self, x1, x2):
         return F.cosine_similarity(x1, x2)
 
-    def eval(self, epoch):
-        for epoch in range(args.eval_epochs):
-            self.eval_trainer.train(epoch)
+    def save(self, model_path, epoch):
+        data = {
+            'model': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'epoch': epoch,
+        }
+        torch.save(data, model_path)
 
-        self.eval_trainer.eval(epoch)
+        self.config.logger.info(f'save model to {model_path}')
+
+    def load(self, model_path):
+        if not os.path.isfile(model_path):
+            return 1
+
+        data = torch.load(model_path)
+        self.model.load_state_dict(data['model'])
+        self.optimizer.load_state_dict(data['optimizer'])
+
+        self.config.logger.info(f'load model from {model_path}')
+
+        return data['epoch'] + 1
 
 
 class LinearEvaluationTrainer():
-    def __init__(self, config, base_model):
+    def __init__(self, config):
         self.config = config
 
-        self.base_model = base_model
-        self.head = LinearEvaluationHead(config)
+        self.device = config.device
+        self.base_model = Model(config).to(self.device)
+        self.head = LinearEvaluationHead(config).to(self.device)
+
+        for param in self.base_model.parameters():
+            param.requires_grad = False
 
         lr = 0.1 * config.batch_size / 256
         self.optimizer = optim.AdamW(self.head.parameters(), lr=lr)
         # self.optimizer = optim.LBFGS(self.head.parameters(), lr=lr)
 
-        self.dataloader_train = DataLoader(LinearEvaluationDataset(config, 'train'),
-                                           batch_size=config.eval_batch_size, shuffle=True)
-        self.dataloader_eval = DataLoader(LinearEvaluationDataset(config, 'test'),
-                                          batch_size=config.eval_batch_size, shuffle=False)
+        train_dataset, eval_dataset = SERIAL_EXECUTOR.run(lambda: 
+            LinearEvaluationDataset(config, 'train'),
+            LinearEvaluationDataset(config, 'test'))
 
-    def train(self, epoch):
+        self.dataloader_train = torch.utils.data.DataLoader(
+            train_dataset,
+            batch_size=self.config.eval_batch_size,
+            shuffle=True,
+            drop_last=True)
+
+        self.dataloader_eval = torch.utils.data.DataLoader(
+            eval_dataset,
+            batch_size=self.config.eval_batch_size,
+            shuffle=False,
+            drop_last=True)
+
+        self.writer = SummaryWriter(log_dir=config.tensorboard_log_dir)
+        self.start_time = time.time()
+
+    def train(self):
+        for epoch in range(1, args.eval_epochs + 1):
+            para_loader = pl.ParallelLoader(self.dataloader_train, [self.device])
+            self.train_loop_fn(para_loader.per_device_loader(self.device), epoch)
+
+            self.config.logger.info('Finished training epoch {}'.format(epoch))
+
+    def train_loop_fn(self, loader, epoch):
         self.base_model.train()
         self.head.train()
 
-        for i, (data, labels) in enumerate(self.dataloader_train):
-            start_time = time.time()
+        for i, (data, labels) in enumerate(loader):
             self.optimizer.zero_grad()
             loss, _ = self.__loss(data, labels)
             loss.backward()
             self.optimizer.step()
 
             if i % self.config.log_interval == 0:
-                elapsed_time = time.time() - start_time
-                self.config.logger.info('eval epoch: {}, step: {}, loss: {:.2f}, time: {:.2f}'.format(epoch, i, loss, elapsed_time))
+                elapsed_time = time.time() - self.start_time
+                template = "eval train epoch: {}, step: {}, Elapsed: {:0.2f} sec, Loss: {:0.3f}"
+                self.config.logger.info(template.format(epoch, i, elapsed_time, loss.item()))
 
     def __loss(self, data, labels):
         with torch.no_grad():
@@ -223,15 +291,17 @@ class LinearEvaluationTrainer():
 
         return F.cross_entropy(output, labels), output
 
+    def eval(self):
+        self.eval_loop_fn(self.dataloader_eval)
+
     @torch.no_grad()
-    def eval(self, epoch):
+    def eval_loop_fn(self, loader):
         self.base_model.eval()
         self.head.eval()
 
         all_labels = torch.empty(0)
         all_preds = torch.empty(0)
         losses = []
-        start_time = time.time()
 
         for i, (data, labels) in enumerate(self.dataloader_eval):
             loss, logits = self.__loss(data, labels)
@@ -241,19 +311,12 @@ class LinearEvaluationTrainer():
             all_labels = torch.cat([all_labels, labels.cpu()])
             all_preds = torch.cat([all_preds, preds.cpu()])
 
-        elapsed_time = time.time() - start_time
+        elapsed_time = time.time() - self.start_time
         average_loss = sum(losses)/len(losses)
-        self.config.logger.info('eval epoch: {}, loss: {:.2f}, time: {:.2f}'.format(epoch, average_loss, elapsed_time))
+        self.config.logger.info('loss: {:.2f}%, time: {:.2f}'.format(average_loss, elapsed_time))
 
         df = pd.DataFrame(metrics.classification_report(all_labels, all_preds, output_dict=True))
         print(tabulate(df, headers='keys', tablefmt="github", floatfmt='.2f'))
-
-        # f1_score = df.loc['f1-score']
-        # micro = f1_score['micro avg'] if 'micro avg' in f1_score else f1_score['accuracy']
-        # macro = f1_score['macro avg']
-        # self.writer.add_scalar('eval/loss', average_loss, epoch, start_time)
-        # self.writer.add_scalar('eval/f1_score_micro(accuracy)', micro, epoch, start_time)
-        # self.writer.add_scalar('eval/f1_score_macro', macro, epoch, start_time)
 
 
 if __name__ == '__main__':
@@ -261,37 +324,43 @@ if __name__ == '__main__':
     parser.add_argument('--cpu', action='store_true', help='use cpu')
     parser.add_argument('--loglevel', default='DEBUG')
     parser.add_argument('--log_interval', type=int, default=1)
-    parser.add_argument('--eval_interval', type=int, default=1)
+    parser.add_argument('--save_interval', type=int, default=1)
     parser.add_argument('--dataroot', default='data', help='path to data directory')
     parser.add_argument('--name', default=None)
     parser.add_argument('--epochs', type=int, default=100, help='epochs for simclr training')
     parser.add_argument('--eval_epochs', type=int, default=90, help='epoch count')
+    parser.add_argument('--warmup_epochs', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=64, help='size of batch for simclr training')
     parser.add_argument('--eval_batch_size', type=int, default=64, help='size of batch for eval')
     parser.add_argument('--n_classes_encoder', type=int, default=2048, help='dimension of encoder output (h)')
     parser.add_argument('--n_classes_eval', type=int, default=10, help='dimension of evaluation output')
     parser.add_argument('--projection_hidden_dim', type=int, default=2048, help='dimension of projection hidden layer')
     parser.add_argument('--projection_output_dim', type=int, default=128, help='dimension of projection output (z)')
+    parser.add_argument('--n_workers', type=int, default=4)
+    parser.add_argument('--n_cores', type=int, default=8)
+    parser.add_argument('--metrics_debug', action='store_true')
+    parser.add_argument('--eval', default=None, choices=['linear'])
     args = parser.parse_args()
-
-    is_cpu = args.cpu or not torch.cuda.is_available()
-    args.device_name = "cpu" if is_cpu else "cuda:0"
-    args.device = torch.device(args.device_name)
 
     logger = setup_logger(name=__name__, level=args.loglevel)
     logger.info(args)
     args.logger = logger
 
+    is_cpu = args.cpu or not torch.cuda.is_available()
+    args.device_name = "cpu" if is_cpu else "cuda"
+    args.device = torch.device(args.device_name)
+
     if args.name is None:
         args.name = str(uuid.uuid4())[:8]
 
     args.tensorboard_log_dir = f'{args.dataroot}/runs/{args.name}'
+    args.model_path = f'{args.dataroot}/{args.name}.pth'
 
-    trainer = Trainer(args)
+    if args.eval is None:
+        trainer = Trainer(args)
+        trainer.train()
+        sys.exit()
 
-    for epoch in range(args.epochs):
-        trainer.train(epoch)
-        if epoch % args.eval_interval == 0:
-            trainer.eval(epoch)
-
-    trainer.eval(epoch)
+    trainer = LinearEvaluationTrainer(args)
+    trainer.train()
+    trainer.eval()
