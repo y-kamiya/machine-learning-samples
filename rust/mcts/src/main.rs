1cb4e2247c6c595d0662d2975093e87472245f69
y-kamiya <y.kamiya0@gmail.com>
2026-08-02 17:41:09 +0900

    simple monte carlo

---
+++ rust/mcts/src/main.rs
@@ -0,0 +1,26 @@
+use rand::prelude::*;
+
+fn simulate(max: i32) -> f32 {
+    let mut count = 0;
+    let mut rng = rand::rng();
+    for _ in 0..max {
+        let x = rng.random_range(0.0..=1.0);
+        let y = rng.random_range(0.0..=1.0);
+        let d = x * x + y * y;
+        if d <= 1.0 {
+            count += 1;
+        }
+    }
+    count as f32 / max as f32 * 4.0
+}
+
+fn monte_carlo() {
+    for n in [100, 1000, 10000, 100000] {
+        let output = simulate(n);
+        println!("sample count {n}: {output}");
+    }
+}
+
+fn main() {
+    monte_carlo();
+}
