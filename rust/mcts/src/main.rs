fd6a3b6b12e4e8448d35cef25249dee2b624a575
y-kamiya <y.kamiya0@gmail.com>
2026-09-02 21:22:17 +0900

    expand

--- rust/mcts/src/main.rs
+++ rust/mcts/src/main.rs
@@ -25,6 +25,11 @@ impl TreeNode {
             state: state,
         }
     }
+
+    fn add_child(&mut self, index: usize) -> usize {
+        self.children.push(index);
+        index
+    }
 }
 
 struct Tree {
@@ -36,8 +41,9 @@ impl Tree {
         Tree { nodes: Vec::new() }
     }
 
-    fn add_node(&mut self, node: TreeNode) {
+    fn add_node(&mut self, node: TreeNode) -> usize {
         self.nodes.push(node);
+        self.nodes.len() - 1
     }
 
     fn root(&self) -> &TreeNode {
@@ -45,23 +51,14 @@ impl Tree {
     }
 
     fn get(&self, index: usize) -> &TreeNode {
-        assert!(0 <= index && index < self.nodes.len());
+        assert!(index < self.nodes.len());
         &self.nodes[index]
     }
 
-    // fn find_best_leaf(&self) -> &TreeNode {
-    //     let mut rng = rand::rng();
-    //     let mut node = self.root();
-    //     while !node.children.is_empty() {
-    //         let score_max = node.children.iter().map(|&n| self.nodes[n].score).fold(f64::NEG_INFINITY, f64::max);
-    //         let best_index = node.children.iter()
-    //             .filter(|n| self.nodes[**n].score == score_max)
-    //             .choose(&mut rng).unwrap();
-    //         node = &self.nodes[*best_index];
-    //     }
-    //
-    //     &node
-    // }
+    fn get_mut(&mut self, index: usize) -> &mut TreeNode {
+        assert!(index < self.nodes.len());
+        &mut self.nodes[index]
+    }
 }
 
 fn actions_not_expanded(tree: &Tree, index: usize, field: &Field) -> Vec<Action> {
@@ -71,7 +68,7 @@ fn actions_not_expanded(tree: &Tree, index: usize, field: &Field) -> Vec<Action>
     let movables = field.movable_actions(node.state);
     for action in movables {
         let to = field.act(node.state, action);
-        let has_state = node.children.iter().any(|n| tree.get(*n).state == to);
+        let has_state = node.children.iter().any(|i| tree.get(*i).state == to);
         if !has_state {
             not_expanded.push(action);
         }
@@ -93,9 +90,9 @@ fn is_fully_expanded(tree: &Tree, index: usize, field: &Field) -> bool {
 
 fn find_best_child(tree: &Tree, children: &Vec<usize>) -> usize {
     let mut rng = rand::rng();
-    let score_max = children.iter().map(|&n| tree.get(n).score).fold(f64::NEG_INFINITY, f64::max);
+    let score_max = children.iter().map(|&i| tree.get(i).score).fold(f64::NEG_INFINITY, f64::max);
     let best_index = children.iter()
-        .filter(|n| tree.get(**n).score == score_max)
+        .filter(|i| tree.get(**i).score == score_max)
         .choose(&mut rng).unwrap();
     *best_index
 }
@@ -109,11 +106,31 @@ fn select(tree: &Tree, field: &Field) -> usize {
     index
 }
 
-// fn expand(tree: &Tree, parent: usize) -> &TreeNode {
-//     // TreeNodeの新規作成
-//     // treeに追加
-//     // parentのchildrenにindex追加
-// }
+fn create_new_node(tree: &Tree, parent_index: usize, field: &Field) -> TreeNode {
+    let parent = tree.get(parent_index);
+    let movables = field.movable_actions(parent.state);
+    let mut next_state = None;
+    for action in movables {
+        let pos = field.act(parent.state, action);
+        let has_node = parent.children.iter().any(|i| tree.get(*i).state == pos);
+        if !has_node {
+            next_state = Some(pos);
+            break;
+        }
+    }
+
+    assert_eq!(next_state, None);
+    let mut node = TreeNode::new(next_state.unwrap());
+    node.parent = Some(parent_index);
+    node
+}
+
+fn expand(tree: &mut Tree, parent_index: usize, field: &Field) -> usize {
+    let new_node = create_new_node(tree, parent_index, field);
+    let new_index = tree.add_node(new_node);
+    let parent = tree.get_mut(parent_index);
+    parent.add_child(new_index)
+}
 
 fn rollout() {
 }
@@ -131,6 +148,9 @@ fn mcts() {
         let index = select(&_tree, &_field);
         let node = _tree.get(index);
         println!("{:?}", node);
+
+        let new_index = expand(&mut _tree, index, &_field);
+        println!("{:?}", new_index);
     }
 
 
